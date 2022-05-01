178fedac805a1afd8d40553b8d52167b7d8c1906
anandbhavsar01 <anandbhavsar01@gmail.com>
2022-05-01 16:21:16 -0700

    Revised code and added notebook

--- preprocessing.py
+++ preprocessing.py
@@ -7,22 +7,22 @@ import regex
 def extract_emojis(input_file):
     emoji_list = []
     emoji_mat = {}
-    comments = input_file.readlines()
-    for line in comments:
-        comment = line[0]
-        data = regex.findall(r'\X', comment)
+    posts = input_file.readlines()
+    for line in posts:
+        post = line[0]
+        data = regex.findall(r'\X', post)
         for word in data:
             if any(char in emoji.EMOJI_DATA for char in word):
                 if word not in emoji_list:
                     emoji_list.append(word)
     
     for key in emoji_list:
-        emoji_mat[key] = np.zeros(len(comments)-1)
+        emoji_mat[key] = np.zeros(len(posts)-1)
 
     i = 0
-    for line in comments:
-        comment = line[0]
-        data = regex.findall(r'\X', comment)
+    for line in posts:
+        post = line[0]
+        data = regex.findall(r'\X', post)
         for word in data:
             for char in word:
                 if emoji_mat.get(char) is not None:
@@ -31,4 +31,6 @@ def extract_emojis(input_file):
     return emoji_mat
 
 f = open('dataset/reddit_wsb.csv', "r")
-print(extract_emojis(f))
+mat = extract_emojis(f)
+for emoji_val in mat.keys():
+    print(emoji_val, np.sum(mat[emoji_val]))
