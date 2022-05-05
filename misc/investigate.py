7b724debf6e63ca0b9343ccad25a68d44cae0b67
anandbhavsar01 <anandbhavsar01@gmail.com>
2022-05-04 20:27:13 -0700

    observations on data

--- misc/investigate.py
+++ misc/investigate.py
@@ -1,11 +1,13 @@
 from re import A
-from pandas import DataFrame, read_csv
+from pandas import DataFrame, read_csv, read_excel
 import pandas as pd
 import sys
 import matplotlib.pyplot as plt
 from collections import Counter
 
 file = read_csv(r'../dataset/reddit_wsb.csv')
+tickers = read_excel(r'../dataset/tickers.xlsx')
+tickers_found = read_csv(r'../dataset/ticker_location.csv')
 
 #sorted_comments = file.sort_values(['score'], ascending=False)
 #print(sorted_comments.head(1))
@@ -21,7 +23,32 @@ file = read_csv(r'../dataset/reddit_wsb.csv')
 #c = Counter(" ".join(file["title"]).lower().split()).most_common(100)
 #print(c)
 
-file['isBodyNan'] = file.isNa()
-
 #print(file['score'].mean())
 #print(file['comms_num'].mean())
+
+# Now we want info about the posts that mention a Ticker
+file = pd.concat([file, tickers_found['Ticker']], axis=1)
+file['isBodyNan'] = file["body"].isnull()
+
+condensed_file = file.dropna(subset=['Ticker'])
+
+#condensed_file.plot(x = 'created', y = 'score')
+#plt.savefig('score_over_time_tick.png')
+#condensed_file.plot(x='created',y='comms_num')
+#plt.savefig('comments_over_time_tick.png')
+#condensed_file.plot(kind='scatter', x='score', y='comms_num')
+#plt.savefig('score_to_comments_tick.png')
+
+#sorted_condensed = condensed_file.sort_values(['score'], ascending=False)
+#print(sorted_condensed.head(1))
+
+only_text = condensed_file.dropna(subset=['body'])
+only_text.plot(x = 'created', y = 'score')
+plt.savefig('score_over_time_text.png')
+only_text.plot(x='created',y='comms_num')
+plt.savefig('comments_over_time_text.png')
+only_text.plot(kind='scatter', x='score', y='comms_num')
+plt.savefig('score_to_comments_text.png')
+
+sorted_text = only_text.sort_values(['score'], ascending=False)
+print(sorted_text.head(1))
