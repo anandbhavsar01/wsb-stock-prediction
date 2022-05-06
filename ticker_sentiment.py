1a6a9d1490e345ae77929da50829bc1ab1b59e75
Lai Xu <laixu@MacBook-Pro-2.attlocal.net>
2022-05-05 23:32:30 -0700

    adding naive_bayes

--- ticker_sentiment.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed May  4 22:52:09 2022
-
-@author: laixu
-"""
-
-overlay the ticker dictionary and get the ticker sentiment overtime
