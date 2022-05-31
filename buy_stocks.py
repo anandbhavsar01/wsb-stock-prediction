434436b0b83a9ae1b14eadb3fb5518a1833544a9
anandbhavsar01 <anandbhavsar01@gmail.com>
2022-05-30 17:39:38 -0700

    added model that checks by sum of sentiment

--- buy_stocks.py
+++ buy_stocks.py
@@ -1,25 +1,23 @@
 import pandas as pd
 
-def run_model():
-    sentiment_data =  pd.read_csv('dataset/sentiment_ticker_by_day.csv')
+def run_model(stocks):
+    sentiment_data =  pd.read_csv('dataset/sentiment_ticker_by_day_sum.csv')
     ticker_data = pd.read_excel('tickerdata_day_open.xlsx')
 
     balance = 0
-    stocks = { 'PLTR':0,'RKT':0,'ONE':0,'AMC':0,'REAL':0,'SPCE':0,'AMD':0,'DD':0,'GME':0,'TSLA':0,'CRSR':0,'RH':0,'BB':0,'CLOV':0,
-    'NOK':0,'AM':0,'WISH':0,'UWMC':0,'BY':0,'MVIS':0,'NIO':0,'APP':0,'SNDL':0,'AAL':0,'TD':0 }
     for i, day in sentiment_data.iterrows():
         if int(day['Day']) in ticker_data.Day.values:
             print('Day', day['Day'])
             for stock in stocks.keys():
                 if day[stock] > 0:
                     # buy stock
-                    stocks[stock] += 1
-                    balance -= ticker_data.loc[ticker_data['Day']==int(day['Day']),stock].values[0]
+                    stocks[stock] += day[stock]
+                    balance -= day[stock] * ticker_data.loc[ticker_data['Day']==int(day['Day']),stock].values[0]
                 elif day[stock] < 0:
                     # sell stock
                     if stocks[stock] > 0:
-                        stocks[stock] -= 1
-                        balance += ticker_data.loc[ticker_data['Day']==int(day['Day']),stock].values[0]
+                        stocks[stock] -= day[stock]
+                        balance += day[stock] * ticker_data.loc[ticker_data['Day']==int(day['Day']),stock].values[0]
                 # else hold
             print(balance, stocks)
     # Now assume we sell all the stocks on the last day
@@ -27,4 +25,8 @@ def run_model():
     for stock in stocks.keys():
         balance += stocks[stock] * last_day[stock]
     print(balance)
-run_model()
+
+stocks = { 'PLTR':0,'RKT':0,'ONE':0,'AMC':0,'REAL':0,'SPCE':0,'AMD':0,'DD':0,'GME':0,'TSLA':0,'CRSR':0,'RH':0,'BB':0,'CLOV':0,
+    'NOK':0,'AM':0,'WISH':0,'UWMC':0,'BY':0,'MVIS':0,'NIO':0,'APP':0,'SNDL':0,'AAL':0,'TD':0 }
+
+run_model(stocks)
