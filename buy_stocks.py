b17b55c6c46eff0cfbe5213e4d138204bb2c1cd2
anandbhavsar01 <anandbhavsar01@gmail.com>
2022-05-30 22:07:32 -0700

    Added one stock to equity

--- buy_stocks.py
+++ buy_stocks.py
@@ -1,6 +1,31 @@
 import pandas as pd
 
 
+def equity_model_ts_one_stock(stocks, sentiment_data, ticker_data):
+    sentiment  = sentiment_data
+    equity_dict = dict()
+    holding_dict  = dict()
+    for day in sentiment_data.index:
+            print('Day', day)
+            equity = 0
+            for stock in stocks.keys():
+                if sentiment.loc[day,stock] > 0:
+                    # buy stock
+                    stocks[stock] += 1
+                elif sentiment.loc[day,stock] < 0:
+                    # sell stock
+                    if stocks[stock] > 0:
+                        stocks[stock] -= 1
+                    else:
+                        stocks[stock] = 0
+                equity += ticker_data.loc[ticker_data['Day']==int(sentiment.loc[day,stock]['Day'])].values[0]
+                # else hold
+                equity_dict[day] = equity
+                holding_dict[day] = stocks
+            print(equity, stocks)
+    print(equity) 
+    return equity_dict,holding_dict
+
 def equity_model_ts(stocks, sentiment_data, ticker_data):
     sentiment  = sentiment_data
     equity_dict = dict()
@@ -14,7 +39,7 @@ def equity_model_ts(stocks, sentiment_data, ticker_data):
                     stocks[stock] += sentiment.loc[day,stock]
                 elif sentiment.loc[day,stock] < 0:
                     # sell stock
-                    if stocks[stock] > sentiment.loc[day,stock]:
+                    if stocks[stock] >= sentiment.loc[day,stock]:
                         stocks[stock] -= sentiment.loc[day,stock]
                     else:
                         stocks[stock] = 0
@@ -89,3 +114,6 @@ sentiment_data =  pd.read_csv('dataset/sentiment_ticker_by_day_sum.csv').shift(1
 equity_dict,holding_dict  = equity_model(stocks, sentiment_data.fillna(0), ticker_data)
 equity_df  = pd.DataFrame(equity_dict, index = ['Portfolio Value']).T
 holding_df  = pd.concat(holding_dict)
+equity_dict, holding_dict = equity_model_ts_one_stock(stocks, sentiment_data.fillna(0), ticker_data)
+equity_df_one_stock  = pd.DataFrame(equity_dict, index = ['Portfolio Value']).T
+holding_df_one_stock  = pd.concat(holding_dict)
